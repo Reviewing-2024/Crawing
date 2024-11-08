a5bd9e60ac5e9f84e8ec50fd8bf277bcd5c9309b
Nahyun <mkkllpm@naver.com>
2024-11-08 14:12:45 +0900

    feat : 크롤링 정보 DB저장 구현

--- pc.py
+++ pc.py
@@ -5,6 +5,17 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname = 'reviewing',
+    user = 'reviewing',
+    password = '1234',
+    host = 'localhost',
+    port = '5432'
+)
+
+cur = conn.cursor()
 
 # Chrome 옵션 설정 (헤드리스로 실행하려면)
 chrome_options = Options()
@@ -37,21 +48,25 @@ while collected_count < target_count:
         for carousel in carousels:
             titles = carousel.find_elements(By.CLASS_NAME, "CourseCard_courseCardTitle__1HQgO")
             imgs = carousel.find_elements(By.CSS_SELECTOR, "img.CourseCard_courseCardImage__XcpZb")
-            categories = carousel.find_elements(By.CLASS_NAME, "TextWithArrowLinkButton_linkButton__NBtyJ")
             urls= carousel.find_elements(By.CLASS_NAME, "CourseCard_courseCardDetailContainer__PnVam")
 
-            for title, img, category, url in zip(titles, imgs, categories, urls):
+            for title, img, url in zip(titles, imgs, urls):
                 if collected_count < target_count:
                     title_text = title.text.strip()
-                    category_text = category.text.strip()
                     url_href = url.get_attribute('href')
                     if title_text:  # 제목이 비어 있지 않을 때만 출력
                         print('-' * 50)
                         print(f"{collected_count + 1}. 강의 제목: {title_text}")
                         print(f"   이미지 URL: {img.get_attribute('src')}")
-                        print(f"   카테고리: {category_text}")
                         print(f"   강의 링크: {url_href}")
                         collected_count += 1
+
+                        cur.execute("""
+                            INSERT INTO course (name, url, thumbnail_image, teacher)
+                            VALUES (%s, %s, %s);
+                        """, (title_text, url_href, img))
+                        
+                        conn.commit()
                 else:
                     break  # 원하는 개수에 도달하면 루프 종료
                 
