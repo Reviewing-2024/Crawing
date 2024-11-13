12017fce6b7542b1d0aa56a9c0ca3c39038049d1
seoyeoning <minalunpark@naver.com>
2024-11-13 16:33:03 +0900

    feat: 강의 url 크롤링 추가

--- inflearn.py
+++ inflearn.py
@@ -7,11 +7,13 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # Chrome 드라이버 생성
 chrome_options = Options()
+userAgent = "user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
+chrome_options.add_argument(userAgent)
 chrome_options.add_argument("--headless")  # Chrome을 headless 모드로 실행
 
 driver = webdriver.Chrome(options=chrome_options)
 
-for page in range(1,4):
+for page in range(1,2):
     
     url = f"https://www.inflearn.com/courses?types=ONLINE&page_number={page}"
     driver.get(url)
@@ -23,30 +25,39 @@ for page in range(1,4):
     req = driver.page_source
     soup = BeautifulSoup(req, "html.parser")
 
-    twoDiv = soup.find_all('ul',class_='css-y21pja mantine-1avyp1d')
+    twoDiv = soup.find_all('ul',class_='css-2ldd65 mantine-1avyp1d')
+
+    count = 0
 
     for i in twoDiv:
         print('=========')
-        imgDivs = i.find_all('div',class_='mantine-AspectRatio-root css-2oqlco mantine-1w8yksd')
+        urls = i.find_all('a')
+        thumbnails = i.find_all('div',class_='mantine-AspectRatio-root css-2oqlco mantine-1w8yksd')
         titles = i.find_all('p',class_='mantine-Text-root css-10bh5qj mantine-b3zn22')
         teachers = i.find_all('p',class_='mantine-Text-root css-1r49xhh mantine-aiouth')
 
-        for imgDiv, title, teacher in zip(imgDivs, titles, teachers):
-            
-            imgOrVideo = imgDiv.find('img')
+        for url, thumbnail, title, teacher in zip(urls, thumbnails, titles, teachers):
+            count += 1
 
-            if imgOrVideo: # 이미지
-                thumnail = imgDiv.find('img')['src']
-            else: # 비디오
-                thumnail = imgDiv.find('source')['src']
+            course_url = url.get('href')
+            
+            imgOrVideo = thumbnail.find('img')
+
+            if imgOrVideo: # 썸네일 이미지
+                course_thumbnail = thumbnail.find('img')['src']
+            else: # 썸네일 비디오
+                course_thumbnail = thumbnail.find('source')['src']
 
             title = title.text
             teacher = teacher.text
 
-            print(thumnail)
+            print(course_url)
+            print(course_thumbnail)
             print(title)
             print(teacher)
             print('----------------------')
+    
+    print(count)
 
 driver.quit()
 
