9ca8ad33b3ea125679ce74f12a74ece6de3033b5
seoyeoning <minalunpark@naver.com>
2024-11-15 13:08:39 +0900

    feat: 강의 slug 추가

--- inflearn.py
+++ inflearn.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import psycopg2
 
+# 로컬 db 연결
 conn = psycopg2.connect(
     host="localhost",       # 데이터베이스 호스트 이름
     database="reviewing",  # 데이터베이스 이름
@@ -27,13 +28,13 @@ driver = webdriver.Chrome(options=chrome_options)
 count = 0
 
 for page in range(1,2):
-    if count >= 30:
+    if count >= 20:
         break
     
     # url = f"https://www.inflearn.com/courses?types=ONLINE&page_number={page}" 전체 강의 조회
     # 분야별 크롤링
-    slug = '' # web-dev, front-end, back-end
-    url = f"https://www.inflearn.com/courses/it-programming/{slug}?types=ONLINE&page_number={page}"
+    category_slug = 'back-end' # web-dev, front-end, back-end
+    url = f"https://www.inflearn.com/courses/it-programming/{category_slug}?types=ONLINE&page_number={page}"
     driver.get(url)
 
     WebDriverWait(driver, 100).until(
@@ -54,12 +55,14 @@ for page in range(1,2):
         teachers = i.find_all('p',class_='mantine-Text-root css-1r49xhh mantine-aiouth')
 
         for url, thumbnail, title, teacher in zip(urls, thumbnails, titles, teachers):
-            if count >= 30:
+            if count >= 20:
                 break
 
             count += 1
 
             course_url = url.get('href')
+
+            course_slug = course_url.split('/')[-1].split('?')[0]
             
             imgOrVideo = thumbnail.find('img')
 
@@ -76,6 +79,7 @@ for page in range(1,2):
 
             print(title)    
             print(course_url)
+            print(course_slug)
             print(thumbnail_image)
             print(thumbnail_video)
             print(teacher)
@@ -83,15 +87,15 @@ for page in range(1,2):
 
             
             cur.execute("""
-            INSERT INTO course (platform_id, category_id, name, url, thumbnail_image, thumbnail_video, teacher)
+            INSERT INTO course (platform_id, category_id, title, url, thumbnail_image, thumbnail_video, teacher,slug)
             VALUES (
                 (SELECT id FROM platform WHERE name = '인프런'),
                 (SELECT c.id FROM category c 
                 INNER JOIN platform p ON p.id = c.platform_id 
                 WHERE p.name = '인프런' AND c.slug = %s),
-                %s, %s, %s, %s, %s
+                %s, %s, %s, %s, %s, %s
             );
-            """, (slug, title, course_url, thumbnail_image, thumbnail_video, teacher))
+            """, (category_slug, title, course_url, thumbnail_image, thumbnail_video, teacher, course_slug))
     
     print(count)
     conn.commit()
