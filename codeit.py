e649554b4c58f7fffa09bb2d41c8c10ba68bb24b
Nahyun <mkkllpm@naver.com>
2024-11-15 10:38:41 +0900

    feat : 카테고리별 DB 저장기능 구현

--- codeit.py
+++ codeit.py
@@ -6,73 +6,72 @@ import time
 import psycopg2
 
 conn = psycopg2.connect(
-        dbname = 'reviewing',
-        user = 'reviewing',
-        password = '1234',
-        host = 'localhost'
-    )
+    dbname='reviewing',
+    user='reviewing',
+    password='1234',
+    host='localhost'
+)
 
 cur = conn.cursor()
 
-# Chrome 옵션 설정 (헤드리스로 실행하려면)
+# Chrome 옵션 설정
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # 화면을 띄우지 않고 실행
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--ignore-certificate-errors")  # SSL 인증서 오류 무시
+chrome_options.add_argument("--ignore-certificate-errors")
 
-# ChromeDriver 경로 설정
-chrome_driver_path = 'C:\\Users\\82109\\crawling\\Crawling\\selenium\\Scripts\\chromedriver.exe' # 크롬 드라이버 경로
+chrome_driver_path = 'C:\\Users\\82109\\crawling\\Crawling\\selenium\\Scripts\\chromedriver.exe'
 service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# FastCampus 카테고리 페이지로 이동
-driver.get('https://www.codeit.kr/roadmaps?slug=web')
+slug = ''  # FRONTEND, BACKEND, FULLSTACK
+url = f"https://www.codeit.kr/explore?page=1&category={slug}&difficulty=&types="
+driver.get(url)
 
 # 페이지 로드 대기
 time.sleep(3)
 
-target_count = 1  # 가져올 강의 수
-collected_count = 0  # 수집된 강의 수
+target_count = 10
+collected_count = 0  
 
-while collected_count < target_count:
-    try:
-        # 강의 카드 요소 찾기
-        carousels = driver.find_elements(By.CLASS_NAME, "ExploreSection_contents__s8NM9")
+try:
+    while collected_count < target_count:
+        carousels = driver.find_elements(By.CLASS_NAME, "TopicList_grid__7bZ8U")
 
         for carousel in carousels:
-            # 요소 선택자 수정
-            titles = carousel.find_elements(By.CLASS_NAME, "CommonExploreItem_title__tNRhO")
-            imgs = carousel.find_elements(By.CSS_SELECTOR, "img.CommonExploreItem_icon__W_3rB")
-            urls = carousel.find_elements(By.CLASS_NAME, "CommonExploreItem_outline__mn3kc.CommonExploreItem_hoverable__9MoEj")  # 링크 요소 확인
+            titles = carousel.find_elements(By.CLASS_NAME, "TopicCommonCard_title__0KrCI")
+            urls = carousel.find_elements(By.CLASS_NAME, "TopicCommonCard_body__3_gHR")
 
-            for title, img, url in zip(titles, imgs, urls):
+            for title, url in zip(titles, urls):
                 if collected_count < target_count:
                     title_text = title.text.strip()
-                    img_src = img.get_attribute('src')
-                    url_href = url.get_attribute('href')  # href 속성 가져오기
-                    if title_text:  # 제목이 비어 있지 않을 때만 출력
+                    url_href = url.get_attribute('href')
+                    if title_text:
                         print('-' * 50)
                         print(f"{collected_count + 1}. 강의 제목: {title_text}")
-                        print(f"   이미지 URL: {img_src}")
                         print(f"   강의 링크: {url_href}")
-                        
-                        cur.execute(
-                            "INSERT INTO course (name, url, thumbnail_image) VALUES (%s, %s, %s)", (title_text, url_href, img_src)
-                        )
-                        
+                        cur.execute("""
+                        INSERT INTO course (platform_id, category_id, title, url)
+                        VALUES (
+                            (SELECT id FROM platform WHERE name = '코드잇'),
+                            (SELECT c.id FROM category c 
+                             INNER JOIN platform p ON p.id = c.platform_id 
+                             WHERE p.name = '코드잇' AND c.slug = %s),
+                            %s, %s
+                        );
+                        """, (slug, title_text, url_href))
                         conn.commit()
                         collected_count += 1
                 else:
-                    break  # 원하는 개수에 도달하면 루프 종료
-                
-    except Exception as e:
-        print(f"오류 발생: {e}")
-        break
+                    break
 
-    finally:
+except Exception as e:
+    print(f"오류 발생: {e}")
+
+finally:
     # 데이터베이스 및 드라이버 종료
-        cur.close()
-        conn.close()
-        driver.quit()
-        print("작업 완료 및 연결 종료")
+    cur.close()
+    conn.close()
+    driver.quit()
+    print("작업 완료 및 연결 종료")
